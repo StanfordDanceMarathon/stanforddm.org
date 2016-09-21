f8e2b1f1800c3bec2aef5adced0aed48a866294a
Tony Bruess <tony@oc.tc>
2016-09-21 19:07:17 +0000

    Finish initial draft

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,50 @@
 $(document).ready(function () {
+    /*
+    var top = true;
+    var transition = false;
+    var cutoff = 300;
+
     $(document).scroll(function() {
         var y = $(this).scrollTop();
-        if (y > 300) {
-            $('.navbar-logo img').fadeIn();
-        } else {
+        if (transition) return;
+
+        if (y > cutoff && top) {
+            transition = true;
+            top = false;
+
+            $('.navbar-logo h2').fadeOut();
+            setTimeout(function (){
+                $('.navbar-logo img').fadeIn();
+                transition = false;
+            }, 400);
+            $('.navbar-inverse').animate({height: "90px"}, 400);
+        } else if (y < cutoff && !top) {
+            transition = true;
+            top = true;
+
             $('.navbar-logo img').fadeOut();
+            setTimeout(function (){
+                $('.navbar-logo h2').fadeIn();
+                transition = false;
+            }, 400);
+            $('.navbar-inverse').animate({height: "110px"}, 400);
         }
     });
+    */
+
+    $(function() {
+        $('a[href*=\\#]:not([href=\\#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+               var target = $(this.hash);
+               target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+               if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 50
+                    }, 400);
+                    return false;
+                }
+            }
+        });
+    });
+
 });
